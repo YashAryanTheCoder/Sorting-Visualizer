71b03feed20b10b9863208cc062d02bdf52005f9
Yash Aryan <73401116+YashAryanTheCoder@users.noreply.github.com>
2023-07-26 16:37:23 +0530

    Update selection_sort.js

--- scripts/selection_sort.js
+++ scripts/selection_sort.js
@@ -1,10 +1,3 @@
-/*
-    *****************
-    DONE BY:-   TUMMALA KETHAN
-    
-    *****************
-*/
-
 function Selection_sort()
 {
     //Setting Time complexities
@@ -59,9 +52,3 @@ function Selection_sort()
     enable_buttons();
 }
 
-/*
-    *****************
-    DONE BY:-   TUMMALA KETHAN
-    
-    *****************
-*/
